fb78e3880a184d9d891526fe39510fc2a9ff5525
raymond <vanrooijenr01@gmail.com>
2023-09-07 23:07:30 +0200

    wallpapers replacement as well as nvim update

--- other/nvim_setup/nvim/lua/ray/remap.lua
+++ other/nvim_setup/nvim/lua/ray/remap.lua
@@ -12,7 +12,16 @@ map('n', '<leader>pm', ':set formatoptions=tcrwa<CR>')
 map('n', '<leader>pn', ':set formatoptions=tc<CR>')
 map('n', '<leader>fw', ':%!clang-format --style=Google<CR>')
 
+-- Move selected line / block of text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Paste over visual selection, without copying selected text
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- Copy to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 
